f2bee0fe3445131ce97b7307795cf2275e607351
Christopher Wood <christopherwood07@gmail.com>
2016-05-23 10:33:19 -0700

    Add setup script and unit test stubs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysafeprime',
+      version='0.1',
+      description='Library to generate cryptographically safe primes',
+      url='http://github.com/chris-wood/pysafeprime',
+      author='Christopher Wood',
+      author_email='chris@caw.fyi',
+      license='MIT',
+      packages=['pysafeprime'],
+      zip_safe=False)
---
+++ tests/pysafeprime_tests.py
@@ -0,0 +1,12 @@
+from nose.tools import *
+
+import pysafeprime
+
+# def setup():
+#     pass
+#
+# def teardown():
+#     pass
+
+def test_stub():
+    pass
